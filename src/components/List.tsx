323f539f3a434079edccf0e53e0db49e4b2d719e
Sean Chou <sean1093@gmail.com>
2025-02-08 12:04:04 +0800

    feat: add utm trace

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -13,6 +13,7 @@ type ListProps = {
 const List: React.FC<ListProps> = ({ domain, blogPosts }) => {
     return (
         <div style={{ width: '100%', height: '100%', overflowY: 'auto' }}>
+            {Object.entries(blogPosts).length === 0 && <div>資料尚未更新</div>}
             {Object.entries(blogPosts).map(([category, posts]) => (
                 <div key={category}>
                     <h2>{category}</h2>
@@ -20,7 +21,7 @@ const List: React.FC<ListProps> = ({ domain, blogPosts }) => {
                         {posts.map((post) => (
                             <li key={post.link}>
                                 <a
-                                    href={`${domain}${post.link}`}
+                                    href={`${domain}${post.link}?utm_source=personal_outline`}
                                     target="_blank"
                                     rel="noopener noreferrer"
                                 >
